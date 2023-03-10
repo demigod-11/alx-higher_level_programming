c506cf51ea334e37b0565291a1b18ffd0e405391
demigod-11 <llousvamel11@gmail.com>
2023-03-10 21:05:02 +0100

    python is fun

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        print("{:d} argument".format(len(sys.argv) - 1))
-        for i, j in enumerate(sys.argv):
+    from sys import argv
+    if len(argv) > 1:
+        print("{:d} argument".format(len(argv) - 1))
+        for i, j in enumerate(argv):
             if i == 0:
                 continue
             print("{:d}: {}".format(i, j))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,5 @@
-import sys
-
 if __name__ == '__main__':
+    import sys
     print(f"Arguments count: {len(sys.argv)}")
     for i, arg in enumerate(sys.argv):
         print(f"Argument {i:>6}: {arg}")
