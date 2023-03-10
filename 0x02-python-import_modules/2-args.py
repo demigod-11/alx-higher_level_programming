d334605f8c7eb8d88002e3193b8da8be88e13d7e
demigod-11 <llousvamel11@gmail.com>
2023-03-10 21:33:06 +0100

    python is fun

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 if __name__ == "__main__":
     from sys import argv
 if len(argv) > 1:
-    print("{:d} argument".format(len(argv) - 1))
+    x = 'arguments' if len(argv) > 2 else 'argument'
+    print("{:d} {}".format(len(argv) - 1, x))
     for i, j in enumerate(argv):
         if i == 0:
             continue
