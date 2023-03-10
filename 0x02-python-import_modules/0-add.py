f468a2b472fa466da706f0f04a663886bb3b4397
demigod-11 <llousvamel11@gmail.com>
2023-03-10 20:51:31 +0100

    python is fun

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    c = add(a, b)
-    print("{} + {} = {} value>".format(a, b, c))
+    print("{} + {} = {} value>".format(a, b, add(a, b)))
