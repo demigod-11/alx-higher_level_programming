44451e18b25ddd96efc2a0012f98fa75d9cc0b83
demigod-11 <llousvamel11@gmail.com>
2023-03-10 21:54:39 +0100

    python is fun

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
-summation = 0
-for i, j in enumerate(sys.argv):
-    if i == 0:
-        continue
-    summation += int(j)
-print(summation)
+if __name__ == "__main__":
+    summation = 0
+    for i, j in enumerate(sys.argv):
+        if i == 0:
+            continue
+        summation += int(j)
+    print(summation)
