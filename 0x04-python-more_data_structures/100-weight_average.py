66cd852a46bbbf4900f39ebb0814540047623fcc
demigod-11 <llousvamel11@gmail.com>
2023-03-15 15:34:23 +0100

    python is fun

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    avg= 0
+    total_weight = 0
+    total_score = 0
+    if not my_list:
+        return (0)
+    for item in my_list:
+        score, weight = item
+        total_weight += weight
+        total_score += score * weight
+    return totalScore / total_weight
