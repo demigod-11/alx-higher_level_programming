007a0134271b459fc2aafda2dd4df0df9de604d7
demigod-11 <llousvamel11@gmail.com>
2023-03-28 15:35:02 +0100

    Class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+    Defines a Class Square
+"""
+
+
+class Square:
+    """
+        Represents a square
+        Attributes:
+            __size (int): size of a side of the square
+    """
+
+    def __init__(self, size=0):
+        """
+            class instantiation
+            Args:
+                 size : class attribute (private)
+            Return: None
+        """
+        self.size = size
+
+    def area(self):
+        """
+            Square Area
+            Args:
+                None
+            Return: int
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+            __size getter
+            Args:
+                None
+            Return: int
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value: int):
+        """
+            __size setter
+            Args:
+                Value (int)
+            Return: None
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size: int = value
+
+    def my_print(self):
+        """
+            prints # amount of area
+            Args:
+                None
+            Return: Str
+        """
+        if self.__size == 0:
+            print()
+            return
+        else:
+            print("#" * self.__size)
