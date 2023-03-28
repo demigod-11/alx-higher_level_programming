9cdcff30a1e29a94f35fa4bf6b9c4400128538b6
demigod-11 <llousvamel11@gmail.com>
2023-03-28 15:00:40 +0100

    Class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    Defines a Class Square
+"""
+
+
+class Square:
+    """
+        Represents a square
+        Attributes:
+            __size (int): size of a side of the square
+    """
+
+    def __init__(self, size=0):
+        """
+            class instantiation
+            Args:
+                 size : class attribute (private)
+            Return: None
+        """
+        self.__size: int = size
+
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    
+    def area(self):
+        """
+            Square Area
+            Args:
+                None
+            Return: int
+        """
+        return self.__size ** 2
