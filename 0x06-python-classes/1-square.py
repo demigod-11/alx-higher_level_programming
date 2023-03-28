8bf7a53bdcc05ca3daf66df55cf06bca4075b7a2
demigod-11 <llousvamel11@gmail.com>
2023-03-28 14:40:12 +0100

    Class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Defines a Class Square
+"""
+
+
+class Square:
+    """
+        A Square class
+    """
+
+    def __init__(size):
+        """
+            class instantiation
+             - size : class attribute (private)
+        """
+        self.__size = size
+
