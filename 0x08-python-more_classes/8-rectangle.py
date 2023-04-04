e876089e5a007e67de8ef6682af70dfe78a57128
demigod-11 <llousvamel11@gmail.com>
2023-04-04 17:22:23 +0100

    xxx

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,7 +75,7 @@ class Rectangle:
 
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
     
     def __del__(self):
         """prints a string when an instance has been deleted"""
